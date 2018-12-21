5e17a93769db4a89c037e959d822698206448170
Gabriel Vigliensoni <gabriel@vigliensoni.com>
2018-12-21 17:03:14 -0500

    New: Create view for inserting data.
    The site root now only shows the list of DB rows. 
    
    The view for enter data is now accessible at /memorial-admin

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Memorial' });
+  res.render('index', { title: 'Page Rwanda Memorial' });
 });
 
 module.exports = router;
